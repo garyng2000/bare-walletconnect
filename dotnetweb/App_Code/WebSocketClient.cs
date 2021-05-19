1651847e25f4f986212cd9f8e8dc50cabc4294a4
gary <github@garyng.com>
2021-05-19 05:40:34 -0700

    add dotnet(c#) connection

---
+++ dotnetweb/App_Code/WebSocketClient.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example
+{
+
+    public class WsClient : IDisposable
+    {
+
+        public void Dispose() => DisconnectAsync().Wait();
+
+        private ClientWebSocket clientWs;
+        private CancellationTokenSource cts;
+        private Action<object> _onData;
+        private Action<object> _onError;
+        private Action _onDone;
+        private bool _cancelOnError;
+        public int ReceiveBufferSize { get; set; } = 8192;
+
+        public async Task<WsClient> ConnectAsync(string url)
+        {
+            if (clientWs != null)
+            {
+                if (clientWs.State == WebSocketState.Open) return this;
+                else clientWs.Dispose();
+            }
+            clientWs = new ClientWebSocket();
+            if (cts != null) cts.Dispose();
+            cts = new CancellationTokenSource();
+            await clientWs.ConnectAsync(new Uri(url), cts.Token);
+            return this;
+        }
+
+        public async Task<WsClient> ListenAsync(Action<object> onData, Action<object> onError = null, Action onDone = null, bool cancelOnError = true)
+        {
+            _onData = onData;
+            _onDone = onDone;
+            _onError = onError;
+            _cancelOnError = cancelOnError;
+            await Task.Factory.StartNew(ReceiveLoop, cts.Token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+            return this;
+        }
+        public async Task DisconnectAsync()
+        {
+            if (clientWs is null) return;
+            // TODO: requests cleanup code, sub-protocol dependent.
+            if (clientWs.State == WebSocketState.Open)
+            {
+                cts.CancelAfter(TimeSpan.FromSeconds(2));
+                await clientWs.CloseOutputAsync(WebSocketCloseStatus.Empty, "", CancellationToken.None);
+                await clientWs.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+            }
+            clientWs.Dispose();
+            clientWs = null;
+            cts.Dispose();
+            cts = null;
+        }
+
+        private async Task ReceiveLoop()
+        {
+            var loopToken = cts.Token;
+            WebSocketReceiveResult receiveResult = null;
+            var buffer = new ArraySegment<byte>(new byte[2048]);
+            try
+            {
+                while (!loopToken.IsCancellationRequested)
+                {
+                    using (var ms = new MemoryStream(ReceiveBufferSize))
+                    {
+                        do
+                        {
+                            receiveResult = await clientWs.ReceiveAsync(buffer, cts.Token);
+                            if (receiveResult.MessageType != WebSocketMessageType.Close)
+                            {
+                                ms.Write(buffer.Array, buffer.Offset, receiveResult.Count);
+                            }
+                            else break;
+                        }
+                        while (!receiveResult.EndOfMessage);
+                        if (_onData != null)
+                        {
+                            try
+                            {
+                                ms.Seek(0, SeekOrigin.Begin);
+                                using (var reader = new StreamReader(ms, Encoding.UTF8))
+                                {
+                                    _onData(await reader.ReadToEndAsync());
+                                }
+                            }
+                            catch { }
+                        }
+                        if (receiveResult.MessageType == WebSocketMessageType.Close) break;
+                    }
+                }
+            }
+            catch (Exception ex) when (ex is TaskCanceledException || ex is Exception)  
+            { 
+                if (_onError != null)
+                {
+                    try
+                    {
+                        _onError(ex);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            finally
+            {
+                if (_onDone != null)
+                {
+                    try
+                    {
+                        _onDone();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private async Task SendMessageAsync(string message)
+        {
+            ArraySegment<byte> bytesToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+            await clientWs.SendAsync(bytesToSend, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+
+}
---
+++ dotnetweb/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using GN.WalletConnect;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        WalletConnect wc = new WalletConnect();
+    }
+
+    protected void cConnectWallet_Click(object sender, EventArgs e)
+    {
+        var t = Task.Run(async () =>
+        {
+            try
+            {
+                var myMeta = new { name = ".NET Dapp", accounts = new List<string>() };
+                var wcSessionRequest = await WCSession.CreateSession("https://bridge.walletconect.org", myMeta);
+                var wcUrl = wcSessionRequest.wcUri;
+
+                //var result = await wcSessionRequest.wcSessionRequest.Value;
+                var x = 1;
+                return wcUrl;
+            } catch (Exception ex)
+            {
+                var r = ex.Message;
+                return null;
+            }
+
+        });
+        t.ConfigureAwait(false);
+        t.Wait();
+        var xx = t.Result;
+        cWcUrl.Text = xx;
+
+    }
+
+    protected void cConnectDApp_Click(object sender, EventArgs e)
+    {
+        var t = Task.Run(async () =>
+        {
+            try
+            {
+                var myMeta = new { name = ".NET Dapp", description = "some description", accounts = new List<string>() };
+                var wcSessionRequest = await WCSession.ConnectSession(cWcUrl.Text, myMeta);
+                var wcSession = wcSessionRequest.Key;
+                var request = wcSessionRequest.Value;
+                await wcSession.SendSessionRequestResponse(request, myMeta, new List<string>(), true, "https://kovan.infura.io", 42, true);
+                return wcSessionRequest;
+            }
+            catch (Exception ex)
+            {
+                var r = ex.Message;
+                throw;
+            }
+
+        });
+        t.ConfigureAwait(false);
+        t.Wait();
+        var xx = t.Result;
+
+    }
+}
